e6764fe1ea26c36c053a657f0311867077e5255f
Daniel Wallace <daniel.wallace.nz@gmail.com>
2015-07-21 19:48:43 +1200

    hook up event for one button

--- app/src/main/java/nz/co/danwallacenz/myappportfolio/MainActivity.java
+++ app/src/main/java/nz/co/danwallacenz/myappportfolio/MainActivity.java
@@ -2,8 +2,10 @@ package nz.co.danwallacenz.myappportfolio;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -34,5 +36,9 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void capstoneButtonTap(View view) {
+        Log.v("capstoneButtonTap",view.toString());
     }
 }
