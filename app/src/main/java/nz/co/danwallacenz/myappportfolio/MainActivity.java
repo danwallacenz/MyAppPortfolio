ff0af3ae8dc05a8919fad40f92ab0d443cf157d4
Daniel Wallace <daniel.wallace.nz@gmail.com>
2015-07-24 19:57:53 +1200

    extract strings to strings.xml, tidy

--- app/src/main/java/nz/co/danwallacenz/myappportfolio/MainActivity.java
+++ app/src/main/java/nz/co/danwallacenz/myappportfolio/MainActivity.java
@@ -3,10 +3,7 @@ package nz.co.danwallacenz.myappportfolio;
 import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
